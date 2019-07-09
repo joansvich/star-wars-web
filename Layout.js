d7831e50486ab926612eb9524da0dc5ba70816ea
Joan Sánchez <joansvich@gmail.com>
2019-07-09 11:27:20 +0200

    Update JS

--- Layout.js
+++ Layout.js
@@ -1,1 +1,33 @@
 'use strict';
+
+class Layout {
+
+  constructor(root, style) {
+    this.root = root;
+    this.style = style
+    this.elements = null;
+    this.header = null;
+    this.main = null;
+    this.footer = null;
+  }
+
+  generate() {
+    this.elements = `
+      <header id="site-header"></header>
+      <main id="site-main"></main>
+      <footer id="site-footer"></footer>
+    `
+    this.render();
+    this.getContainers();
+  }
+
+  render() {
+    this.root.innerHTML = this.elements;
+  }
+
+  getContainers() {
+    this.header = document.querySelector('#site-header');
+    this.main = document.querySelector('#site-main');
+    this.footer = document.querySelector('#site-footer');
+  }
+}
--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const main = () => {
+  const ENTRY_POINT = '/'
+  let layoutInstance = null;
+  let navbarInstance = null;
+  let links = [
+    { name: 'Home', url: '/' },
+    { name: 'Movies', url: '/movies' }
+  ]
+  let rootElement = document.querySelector('#root');
+
+
+  const generateLayout = () => {
+    layoutInstance = new Layout(rootElement);
+    layoutInstance.generate();
+  }
+
+  const generateNavbar = () => {
+    navbarInstance = new Navbar(layoutInstance.header, links);
+  }
+
+  generateLayout();
+  generateNavbar();
+}
+
+window.addEventListener('load', main);
